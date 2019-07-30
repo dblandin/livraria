88cc1d0871f9a624c34f746871dfbd5629da76f8
Devon Blandin <dblandin@gmail.com>
2019-07-30 10:45:53 -0400

    Add TodayILearned to GraphQL schema

--- api/index.js
+++ api/index.js
@@ -1,17 +1,48 @@
 const { ApolloServer, gql } = require('apollo-server-micro')
 const { buildFederatedSchema } = require('@apollo/federation')
-
+const github = require('@octokit/graphql')
 
 const typeDefs = gql`
+  type TodayILearnedPost {
+    body: String
+    url: String
+  }
+
   type Query {
     hello: String
+    TodayILearned: [TodayILearnedPost]
   }
 `
+
+async function fetchIssues() {
+  const query = `{
+    repository(owner:"dblandin", name:"til") {
+      issues(last:3) {
+        edges {
+          node {
+            bodyHTML
+          }
+        }
+      }
+    }
+  }`
+
+  const { repository } = await github(query, {
+    headers: {
+      authorization: `token ${process.env.GITHUB_API_TOKEN}`
+    }
+  })
+
+  return repository.issues.edges
+}
 
 const resolvers = {
   Query: {
     hello: (root, args, context) => {
       return "Hello world!"
+    },
+    TodayILearned: (root, args, context) => {
+      return fetchIssues()
     }
   }
 }
