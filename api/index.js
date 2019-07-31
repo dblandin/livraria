4267a30866cb5bf2f39584c6e8af8e81aa22c4ed
Devon Blandin <dblandin@gmail.com>
2019-07-30 23:57:13 -0400

    Add simple resume to GraphQL api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,111 @@ const { buildFederatedSchema } = require('@apollo/federation')
 const github = require('@octokit/graphql')
 
 const typeDefs = gql`
+  type Reference {
+    name: String
+    reference: String
+  }
+
+  type Interest {
+    name: String
+    keywords: [String]
+  }
+
+  type Language {
+    language: String
+    fluency: String
+  }
+
+  type Skill {
+    name: String
+    level: String
+    keywords: [String]
+  }
+
+  type Publication {
+    name: String
+    publisher: String
+    releaseDate: String
+    website: String
+    summary: String
+  }
+
+  type Award {
+    title: String
+    date: String
+    awarder: String
+    summary: String
+  }
+
+  type Education {
+    institution: String
+    area: String
+    studyType: String
+    startDate: String
+    endDate: String
+    gpa: String
+    courses: [String]
+  }
+
+  type Volunteer {
+    organization: String
+    position: String
+    website: String
+    startDate: String
+    endDate: String
+    summary: String
+    highlights: [String]
+  }
+
+  type Work {
+    company: String
+    position: String
+    website: String
+    startDate: String
+    endDate: String
+    summary: String
+    highlights: [String]
+  }
+
+  type Profiles {
+    network: String
+    username: String
+    url: String
+  }
+
+  type Location {
+    address: String
+    postalCode: String
+    city: String
+    countryCode: String
+    region: String
+  }
+
+  type Basics {
+    name: String
+    label: String
+    picture: String
+    email: String
+    phone: String
+    website: String
+    summary: String
+    profiles: [Profiles]
+    location: Location
+  }
+
+  type Resume {
+    references: [Reference]
+    interests: [Interest]
+    languages: [Language]
+    skills: [Skill]
+    publications: [Publication]
+    awards: [Award]
+    education: [Education]
+    volunteer: [Volunteer]
+    work: [Work]
+    basics: Basics
+  }
+
   type TodayILearnedPost {
     body: String
     url: String
@@ -10,6 +115,7 @@ const typeDefs = gql`
 
   type Query {
     hello: String
+    resume: Resume
     TodayILearned: [TodayILearnedPost]
   }
 `
@@ -43,6 +149,10 @@ const resolvers = {
     },
     TodayILearned: (root, args, context) => {
       return fetchIssues()
+    },
+    resume: (root, args, context) => {
+      const resume = require('./resume');
+      return resume
     }
   }
 }
